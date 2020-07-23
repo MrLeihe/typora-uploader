e56e7979d8037c08f7b435ff46321460e2d50ab5
stone <1484542698@qq.com>
2020-07-23 15:32:36 +0800

    fix: upload error when insert mulit file or screenshot

--- bin/juejin-uploader.sh
+++ bin/juejin-uploader.sh
@@ -3,18 +3,36 @@
 
 # 初始化下标
 index=0
+# 初始化数组
+list=()
 
 # 上传图片
 for item in $@; do
     # 将转义的空格 %20 还原 
-    item=`echo "$item" | sed 's/%20/ /g'`
+    item=`echo "$item" | sed 's/%20/\ /g'`
     # 上传结果
     result=`curl -H "Expect:" -F "file=@$item" https://cdn-ms.juejin.im/v1/upload\?bucket\=gold-user-assets`
     # 添加图片 https 地址到数组，字符截取、去除双引号
-    list[$index]=`echo $result | awk -F '"https":' '{ print $2 }' | awk -F '},' '{ print $1 }' | sed 's/\"//g'`
-    # 更新下标
-    index=`expr $index + 1`
+    imagePath=`echo $result | awk -F '"https":' '{ print $2 }' | awk -F '},' '{ print $1 }' | sed 's/\"//g'`
+    # 检测图片地址是否不为空
+    if [ -n "$imagePath" ]
+    then
+        list[$index]=$imagePath
+        # 更新下标
+        index=`expr $index + 1`
+    else 
+        echo "字符串长度为0"
+    fi
 done
+
+# 如果 list 数组为空
+if [ $index -eq 0  ]
+then
+    filePath=$@
+    result=`curl -H "Expect:" -F "file=@$filePath" https://cdn-ms.juejin.im/v1/upload\?bucket\=gold-user-assets`
+    # 添加图片 https 地址到数组，字符截取、去除双引号
+    list[$index]=`echo $result | awk -F '"https":' '{ print $2 }' | awk -F '},' '{ print $1 }' | sed 's/\"//g'`
+fi
 
 echo "Upload Success:"
 # 遍历图片地址列表
